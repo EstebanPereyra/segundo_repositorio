1d8a399dd4c1062def3542b0aa657d04af7dbe6a
Esteban Pereyra <esteba2000ar@hotmail.com>
2021-07-28 10:13:16 -0300

    cree un componente en src/componentes/ItemCount.js, que permite sumar, restar y agregar al carrito un producto determinado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Header from "./componentes/Header.js"
 import ItemListContainer from "./componentes/ItemListContainer.js"
+import ItemCount from "./componentes/ItemCount.js"
 
 const App = () => {
+
+    const onAdd = (cantidad) => {
+        console.log(`Has agregado al carrito ${cantidad} remera/s`)
+    }
+
     return (
     <>
         <Header/>       
         <ItemListContainer persona="Juan" />
+        <ItemCount stock={5} initial={1} onAdd={onAdd}/>
     </>
 )}
 export default App
